6be99f3c06a6a6269d369472e1bd74bfa74e32b0
Hari Nikesh R <82016724+Hari-Nikesh-R@users.noreply.github.com>
2021-12-29 08:54:25 +0530

    Update Bubble_sort.c

--- Sorting_Algorithm/Bubble_sort.c
+++ Sorting_Algorithm/Bubble_sort.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main(){
-   int a[]={10,5,20,54,23,0,2,99,51};
-   int n=sizeof(a)/sizeof(a[0]);
+   int n;
+   scanf("%d",&n);
+   int arr[n];
    for(int i=0;i<n-1;i++){
       for(int j=0;j<n-i-1;j++){
-         if(a[j]>a[j+1]){
-              int temp=a[j];
-              a[j]=a[j+1];
-              a[j+1]=temp;
+         if(arr[j]>arr[j+1]){
+              int temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
            }
        }
    }
    for(int i=0;i<n;i++)
-   printf("%d ",a[i]);
+      printf("%d ",arr[i]);
 } 
