15b235bc3d7d9ead66bc2d7232ecbedc9d4920bc
Hari Nikesh R <82016724+Hari-Nikesh-R@users.noreply.github.com>
2021-12-29 15:30:03 +0530

    Add files via upload

---
+++ Stack and Queue/Queue_using_Stacks.java
@@ -0,0 +1,30 @@
+public class MyQueue {
+
+Stack<Integer> stack1 = new Stack();
+Stack<Integer> stack2 = new Stack();
+
+public void push(int x) {
+  while (!stack2.isEmpty())
+    stack1.push(stack2.pop());
+  
+  stack1.push(x);
+}
+
+public int pop() {
+  while (!stack1.isEmpty())
+    s2.push(stack1.pop());
+  
+  return stack2.pop();
+}
+
+public int peek() {
+  while (!stack1.isEmpty())
+    stack2.push(stack1.pop());
+  
+  return stack2.peek();
+}
+
+public boolean empty() {
+  return stack1.isEmpty() && stack2.isEmpty();
+}
+}
